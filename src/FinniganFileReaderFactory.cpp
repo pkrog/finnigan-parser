d7c433d7202b26ae767a16143ef1522d4c9d3076
Pierrick Roger Mele <pierrick.rogermele@cea.fr>
2016-02-24 22:56:48 +0100

    Write first lines of reader.

---
+++ src/FinniganFileReaderFactory.cpp
@@ -0,0 +1,25 @@
+#include "FinniganFileReaderFactory.hpp"
+#include "FinniganFileReader66.hpp"
+#include <iostream>
+#include <ifstream>
+#include <cstdint>
+
+using namespace org::openscience::ms;
+
+///////////////////////////////
+// MAKE FINNIGAN FILE READER //
+///////////////////////////////
+
+FinniganFileReader* make_finnigan_file_reader(const char* file) {
+
+	/* Read file type in header */
+	uint16_t magic;
+	uint32_t version;
+	std::ifstream ifs(file, std::ios_base::binary);
+	ifs >> magic;
+	std::wcout << L"MAGIC = " << magic << std::endl;
+
+	/* Read version in header */
+
+	/* Create parser instance */
+}
---
+++ src/FinniganFileReaderFactory.hpp
@@ -0,0 +1,13 @@
+#ifndef FINNIGAN_FILE_READER_FACTORY
+#define FINNIGAN_FILE_READER_FACTORY
+
+namespace org::openscience::ms {
+
+	class FinniganFileReader;
+
+	FinniganFileReader* make_finnigan_file_reader(const char* file);
+
+	};
+}
+
+#endif // FINNIGAN_FILE_READER_FACTORY
